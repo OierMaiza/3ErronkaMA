273d966ee248b81a36db83fc2850b96124d8547b
Oier Maiza <oie_mai_uga@goierrieskola.org>
2026-03-22 11:46:38 +0100

    Jokalariak eta Bikotea klaseak sortuta

---
+++ Bikotea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuskalMus
+{
+    public class Bikotea
+    {
+        public int id { get; set; }
+        public string izena { get; set; }
+        public Bikotea(int id, string izena)
+        {
+            this.id = id;
+            this.izena = izena;
+        }
+    }
+}
---
+++ Jokalariak.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuskalMus
+{
+public class Jokalariak
+    {
+        public string izena { get; set; }
+        public string abizena { get; set; }
+        public string emaila { get; set; }
+        public string telefonoa { get; set; }
+        public string herria { get; set; }
+        public string pasahitza { get; set; }
+
+        public Jokalariak(string izena, string abizena, string emaila, string telefonoa, string herria, string pasahitza)
+        {
+            this.izena = izena;
+            this.abizena = abizena;
+            this.emaila = emaila;
+            this.telefonoa = telefonoa;
+            this.herria = herria;
+            this.pasahitza = pasahitza;
+        }
+       
+
+    }
+}
