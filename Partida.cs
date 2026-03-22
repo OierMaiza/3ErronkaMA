4352513361635da9d6e62b4917c23eebd7a09144
Oier Maiza <oie_mai_uga@goierrieskola.org>
2026-03-22 11:48:30 +0100

    Partida, Txapelketa eta Program sortuta

---
+++ Partida.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuskalMus
+{
+    public class Partida
+    {
+
+        public int id { get; set; }
+        public int faseZenbakia { get; set; }
+        public int posizioa { get; set; }
+        public Bikotea BikoteaA { get; set; }
+        public Bikotea BikoteaB { get; set; }
+        public Bikotea Irabazlea { get; set; }
+        public string Egoera { get; set; }
+
+        public Partida(int id, int faseZenbakia, int posizioa, Bikotea bikoteaA, Bikotea bikoteaB, Bikotea irabazlea, String egoera)
+        {
+            this.id = id;
+            this.faseZenbakia = faseZenbakia;
+            this.posizioa = posizioa;
+            this.BikoteaA = bikoteaA;
+            this.BikoteaB = bikoteaB;
+            this.Irabazlea = irabazlea;
+            this.Egoera = egoera;
+        }
+    }
+}
