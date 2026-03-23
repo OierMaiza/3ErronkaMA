169404e78f98ccf45ed4ef8bcab370a2ff8fd0e9
Oskitz <osk_goe_urr@goierrieskola.org>
2026-03-23 09:00:58 +0100

    Mysql-rekin konexioa eginda

---
+++ Konexioa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace EuskalMus
+{
+    public class Konexioa
+    {
+        private static string url = "Server=localhost;Port=3306;Database=3erronka;Uid=root;Pwd=1MG32025;SslMode=Disabled;";
+        public MySqlConnection getConnection()
+        {
+            MySqlConnection con = null;
+            try
+            {
+                con = new MySqlConnection(url);
+                con.Open();
+                MessageBox.Show("MySQL konexioa eginda", "Konexioa",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Konexio errorea:\n" + e.Message, "Errorea",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return con;
+        }
+    }
+}
